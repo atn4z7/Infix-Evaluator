ccb72bb262b4bca12669ffc4c184545a50d6f6b8
atn4z7 <atn4z7@mail.umkc.edu>
2014-10-24 16:58:12 -0500

    Create Token.cpp

---
+++ Token.cpp
@@ -0,0 +1,16 @@
+
+#include "Token.h"
+
+Token::Token(){}
+Token::Token(std::string Data, int Index, int Length){
+	data= Data;
+	index = Index;
+	length = Length;
+}
+Token& Token::operator=(Token rhs)
+{
+  data = rhs.data;
+  index = rhs.index;
+  length = rhs.length;
+  return *this;
+}
