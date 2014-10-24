fc5aaf7e20e9f5c4d9189d1ff188530c0051306e
atn4z7 <atn4z7@mail.umkc.edu>
2014-10-24 16:57:30 -0500

    Create Syntax_Error.h

---
+++ Syntax_Error.h
@@ -0,0 +1,24 @@
+#ifndef SYNTAX_ERROR_H
+#define SYNTAX_ERROR_H
+
+#include <string>
+#include "Token.h"
+#include <iostream>
+
+
+class Syntax_Error{
+	public:
+		Syntax_Error();
+		//this constructor takes a token in order to print out the error correctly based on the token index and length
+		Syntax_Error(std::string msg, Token token);
+		//function to print out where the error occurs in the expression
+		void Display();
+	private:
+		std::string error;
+		//index of the token in the original expression
+		int index;
+		//length of the token
+		int length;
+};
+
+#endif
