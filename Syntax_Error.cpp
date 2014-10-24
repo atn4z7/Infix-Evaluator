004b0bfe2cb5765ceb02f9e00667400047ecd24a
atn4z7 <atn4z7@mail.umkc.edu>
2014-10-24 16:57:43 -0500

    Create Syntax_Error.cpp

---
+++ Syntax_Error.cpp
@@ -0,0 +1,21 @@
+#include "Syntax_Error.h"
+
+Syntax_Error::Syntax_Error(){}
+Syntax_Error::Syntax_Error(std::string msg,Token token){
+	error = msg;
+	index = token.index;
+	length = token.length;
+}
+
+void Syntax_Error::Display(){
+	//go to the index by displaying multiple " "
+	for (int a = 0; a < index; a++){
+		std::cout << " ";
+	}
+	//print out '^' to indicate where the error occurs
+	for (int a = 0; a < length; a++){
+		std::cout << "^";
+	}
+	//print out the error message
+	std::cout << " " << error << std::endl;
+}
