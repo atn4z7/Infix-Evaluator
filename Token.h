8a29f1f1f2c7a819134855f58a01cb19f0eedf7e
Glaf43 <hdnf39@mail.umkc.edu>
2014-10-30 09:02:16 -0500

    Update Token.h

--- Token.h
+++ Token.h
@@ -1,22 +1,26 @@
 #ifndef TOKEN_H
 #define TOKEN_H
-
 #include <string>
-
 class Token{
 public:
 	Token();
-	Token(std::string Data,  int Index=0, int Length=0);
+	Token(std::string Data, int Index=0, int Length=0);
 	//overloading assignment operator
 	Token& operator=(Token rhs);
 	//data contains the operator string
+	std::string getData();
+	int getLength();
+	int getIndex();
+	void setData(std::string newData);
+	void setLength(int newLength);
+	void setIndex(int newIndex);
+
+private:
 	std::string data;
 	//the index of the operator string in the original expression
 	int index;
 	//the length of the operator string
 	int length;
+
 };
-
-
-
 #endif
