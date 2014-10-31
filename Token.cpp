4d99d4ef0798890cb8391d5481ea2613c4c36959
atn4z7 <atn4z7@mail.umkc.edu>
2014-10-31 14:41:45 -0500

    Update Token.cpp

--- Token.cpp
+++ Token.cpp
@@ -1,11 +1,14 @@
 #include "Token.h"
 
+
 Token::Token(){}
+
 Token::Token(std::string Data, int Index, int Length){
 	data= Data;
 	index = Index;
 	length = Length;
 }
+
 Token& Token::operator=(Token rhs)
 {
 	data = rhs.data;
@@ -13,21 +16,27 @@ Token& Token::operator=(Token rhs)
 	length = rhs.length;
 	return *this;
 }
+
 std::string Token::getData(){
 	return data;
 }
+
 void Token::setData(std::string newData){
 	data=newData;
 }
+
 int Token::getLength(){
 	return length;
 }
+
 int Token::getIndex(){
 	return index;
 }
+
 void Token::setLength(int newLength){
 	length=newLength;
 }
+
 void Token::setIndex(int newIndex){
 	index=newIndex;
 }
